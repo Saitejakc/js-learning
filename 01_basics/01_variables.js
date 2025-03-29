7cbc287cb9ea137aa81bc44f52fcfdc7e6a6adc0
saiteja <saiteja@hebbale.academy>
2025-03-29 11:40:43 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553;
+let accountEmail = "monk@gmail.com";
+var accountPassword = "12345";
+// accountCity = "Hubli";
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
